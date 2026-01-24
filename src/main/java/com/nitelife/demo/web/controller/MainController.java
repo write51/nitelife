5a4ba88d1d03311ae6b20e3c12bda702fc60d33e
dev <dev@oss.unist.hr>
2026-01-24 19:49:51 +0100

    calculate day before events and tommorow events

--- src/main/java/com/nitelife/demo/web/controller/MainController.java
+++ src/main/java/com/nitelife/demo/web/controller/MainController.java
@@ -5,16 +5,49 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 @Controller
 public class MainController {
 
     private final EventService eventService;
 
+    private final String CURRENT_DATE = "2025-02-10";
+
     public MainController(EventService eventService) {
         this.eventService = eventService;
     }
+
+    private String getBackLink(String currentDateString) throws ParseException {
+
+        Date currentDate = new SimpleDateFormat("yyyy-MM-dd").parse(currentDateString);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        calendar.add(Calendar.DAY_OF_YEAR, -1);
+
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        return format.format(calendar.getTime()).replace("-", "/");
+    }
+
+    private String getForwardLink(String currentDateString) throws ParseException {
+
+            Date currentDate = new SimpleDateFormat("yyyy-MM-dd").parse(currentDateString);
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(currentDate);
+            calendar.add(Calendar.DAY_OF_YEAR, 1);
+
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+            return format.format(calendar.getTime()).replace("-", "/");
+    }
+
 
     @GetMapping("/")
     public String redirectToIndex(final Model model) {
@@ -23,6 +56,12 @@ public class MainController {
             model.addAttribute("events1", this.eventService.getByDate(new SimpleDateFormat("yyyy-MM-dd").parse("2025-01-02")));
             model.addAttribute("events2", this.eventService.getByDate(new SimpleDateFormat("yyyy-MM-dd").parse("2025-01-03")));
             model.addAttribute("events3", this.eventService.getByDate(new SimpleDateFormat("yyyy-MM-dd").parse("2025-01-04")));
+
+            model.addAttribute("backLink", "http://localhost:8080/api/events/" + getBackLink("2025-01-01"));
+            model.addAttribute("forwardLink", "http://localhost:8080/api/events/" + getForwardLink("2025-01-01"));
+
+
+
             return "index";
         } catch (Exception e) {
             throw new RuntimeException(e);
