2a5710c5fbf720888244d83715aba062536d8b5f
dev <dev@oss.unist.hr>
2026-01-24 14:35:51 +0100

    main page init

--- src/main/java/com/nitelife/demo/web/controller/MainController.java
+++ src/main/java/com/nitelife/demo/web/controller/MainController.java
@@ -1,13 +1,30 @@
 package com.nitelife.demo.web.controller;
 
+import com.nitelife.demo.business.service.EventService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
 
 @Controller
 public class MainController {
 
+    private final EventService eventService;
+
+    public MainController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
     @GetMapping("/")
-    public String redirectToIndex() {
-        return "index";
+    public String redirectToIndex(final Model model) {
+        try {
+            model.addAttribute("events0", this.eventService.getByDate(new SimpleDateFormat("yyyy-MM-dd").parse("2025-01-01")));
+            model.addAttribute("events1", this.eventService.getByDate(new SimpleDateFormat("yyyy-MM-dd").parse("2025-01-02")));
+            model.addAttribute("events2", this.eventService.getByDate(new SimpleDateFormat("yyyy-MM-dd").parse("2025-01-03")));
+            return "index";
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
