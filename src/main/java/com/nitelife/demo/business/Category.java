658b3590b40c2cffab85f7ab7c4a5b683c050c26
dev <dev@oss.unist.hr>
2026-01-24 15:05:27 +0100

    update

---
+++ src/main/java/com/nitelife/demo/business/Category.java
@@ -0,0 +1,52 @@
+/*
+package com.nitelife.demo.business;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+
+*************************
+Edukacija
+Film
+Izložba
+Mjuzza
+Ostalo
+Predstava
+Sport
+*************************
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, Date date, String time, String location, String price, String category, String description, String link) {
+        this.id = id;
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+
+
+*/
