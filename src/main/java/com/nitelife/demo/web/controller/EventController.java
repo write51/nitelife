e74c22cec4c4cbf2dbc41ec9fb7a9eb6c8b1cdff
dev <dev@oss.unist.hr>
2026-01-24 20:00:44 +0100

    calculate back and forward events and display

--- src/main/java/com/nitelife/demo/web/controller/EventController.java
+++ src/main/java/com/nitelife/demo/web/controller/EventController.java
@@ -7,7 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -36,17 +40,38 @@ public class EventController {
         }
     }
 
-    @GetMapping("/{year}/{month}/{day}")
+/*    @GetMapping("/{year}/{month}/{day}")
     @ResponseBody
-    public List<Event> getEventByDate(@PathVariable String year, @PathVariable String month, @PathVariable String day) {
+    public String getEventByDate(@PathVariable String year, @PathVariable String month, @PathVariable String day, final Model model) {
         try {
             String date = year + "-" + month + "-" + day;
             List<Event> events = eventService.getByDate(new SimpleDateFormat("yyyy-MM-dd").parse(date));
-            return events;
+            return "date";
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }*/
+
+    @GetMapping("/{year}/{month}/{day}")
+    public String getEventByDate(@PathVariable String year, @PathVariable String month, @PathVariable String day, final Model model) {
+        try {
+            String date = year + "-" + month + "-" + day;
+
+            model.addAttribute("events0", this.eventService.getByDate(new SimpleDateFormat("yyyy-MM-dd").parse(date)));
+            model.addAttribute("events1", this.eventService.getByDate(new SimpleDateFormat("yyyy-MM-dd").parse(getForwardLinkNoReplace(date))));
+            model.addAttribute("events2", this.eventService.getByDate(new SimpleDateFormat("yyyy-MM-dd").parse(getForwardLinkNoReplace(getForwardLinkNoReplace(date)))));
+            model.addAttribute("events3", this.eventService.getByDate(new SimpleDateFormat("yyyy-MM-dd").parse(getForwardLinkNoReplace(getForwardLinkNoReplace(getForwardLinkNoReplace(date))))));
+
+            model.addAttribute("backLink", "http://localhost:8080/api/events/" + getBackLink(date));
+            model.addAttribute("forwardLink", "http://localhost:8080/api/events/" + getForwardLink(date));
+
+            return "date";
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
+
 
     @PostMapping
     Event createEvent(@RequestBody Event event) {
@@ -61,6 +86,47 @@ public class EventController {
     @DeleteMapping("/{id}")
     void deleteEvent(@PathVariable Long id) {
         eventService.delete(id);
+    }
+
+    private String getBackLink(String currentDateString) throws ParseException {
+
+        Date currentDate = new SimpleDateFormat("yyyy-MM-dd").parse(currentDateString);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        calendar.add(Calendar.DAY_OF_YEAR, -1);
+
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        return format.format(calendar.getTime()).replace("-", "/");
+    }
+
+
+
+    private String getForwardLink(String currentDateString) throws ParseException {
+
+        Date currentDate = new SimpleDateFormat("yyyy-MM-dd").parse(currentDateString);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        return format.format(calendar.getTime()).replace("-", "/");
+    }
+
+    private String getForwardLinkNoReplace(String currentDateString) throws ParseException {
+
+        Date currentDate = new SimpleDateFormat("yyyy-MM-dd").parse(currentDateString);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        return format.format(calendar.getTime());
     }
 
 }
